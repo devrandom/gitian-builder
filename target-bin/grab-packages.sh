7d1e7c5419121ed4e703902cce2c522fcc7097d3
Devrandom <c1.devrandom@niftybox.net>
2015-03-23 19:46:44 -0700

    trusty fixes, remove legacy grub
    fixes #84

--- target-bin/grab-packages.sh
+++ target-bin/grab-packages.sh
@@ -8,5 +8,8 @@ cd /var/cache/apt/archives
 
 #apt-get clean
 
+# remove obsolete grub, it causes package dependency issues
+apt-get -y purge grub > /dev/null || true
+
 dpkg-query -W -f '${Package}\n' | xargs -n 50 apt-get install --reinstall -y -d  > /dev/null
 sha256sum *.deb | sort --key 2
