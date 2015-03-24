81bf5d70252363a95cb75eea70f8d1d129948013
Devrandom <c1.devrandom@niftybox.net>
2015-03-23 20:47:39 -0700

    ensure guest upgrade occurs before attempting to get manifest
    fixes #85

--- target-bin/grab-packages.sh
+++ target-bin/grab-packages.sh
@@ -6,10 +6,7 @@ set -e
 
 cd /var/cache/apt/archives
 
-#apt-get clean
-
-# remove obsolete grub, it causes package dependency issues
-apt-get -y purge grub > /dev/null || true
-
-dpkg-query -W -f '${Package}\n' | xargs -n 50 apt-get install --reinstall -y -d  > /dev/null
+# make sure all packages with installed versions are downloaded
+dpkg-query -W -f '${Package}=${Version}\n' | xargs -n 50 apt-get install -q --reinstall -y -d > /tmp/download.log
+grep "cannot be downloaded" /tmp/download.log && { echo Could not download some packages, please run gbuild --upgrade 1>&2 ; exit 1 ; }
 sha256sum *.deb | sort --key 2
